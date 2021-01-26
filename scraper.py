8caaab4d602dc70392bdf7ce72ad47083bde2f0a
Mark Brough <mark.brough@gmail.com>
2021-01-26 15:14:55 +0100

    Get FR version of data

--- scraper.py
+++ scraper.py
@@ -7,24 +7,30 @@ import re
 import csv
 
 OECD_STATS_URL="https://stats.oecd.org/"
-CRS_URL="https://stats.oecd.org/viewhtml.aspx?datasetcode=CRS1&lang=en"
+CRS_URL="https://stats.oecd.org/viewhtml.aspx?datasetcode=CRS1&lang={}"
 CRS_CODES_URL="https://stats.oecd.org/ModalDimWebTree.aspx?DimensionCode=SECTOR&SubSessionId={}&Random=0.2017416214402572"
 
-s = requests.Session()
-s.headers.update({'User-Agent': 'Mozilla/5.0'})
-req = s.get(OECD_STATS_URL)
-req_crs = s.get(CRS_URL)
-doc = html.fromstring(req_crs.text)
-jssid = doc.xpath('//input[@name="_ctl0:_ctl0:cphCentre:ContentPlaceHolder1:TBSubSessionId"]')[0].get('value')
-req_crs_codes=s.get(CRS_CODES_URL.format(jssid))
-doc_crs_codes = html.fromstring(req_crs_codes.text)
 
 class CodesGetter():
-    def __init__(self, doc):
-        self.doc = doc
+    def get_doc(self):
+        s = requests.Session()
+        s.headers.update({'User-Agent': 'Mozilla/5.0'})
+        req = s.get(OECD_STATS_URL)
+        req_crs = s.get(CRS_URL.format(self.lang))
+        doc = html.fromstring(req_crs.text)
+        jssid = doc.xpath('//input[@name="_ctl0:_ctl0:cphCentre:ContentPlaceHolder1:TBSubSessionId"]')[0].get('value')
+        req_crs_codes=s.get(CRS_CODES_URL.format(jssid))
+        doc_crs_codes = html.fromstring(req_crs_codes.text)
+        return doc_crs_codes
+
+
+    def __init__(self, lang='en'):
+        self.lang = lang
+        self.doc = self.get_doc()
         self.top_categories = self.doc.xpath('//div[@id="M_WebTreeDimMembers_1_1"]')[0].xpath("*")
         self.codes = []
         self.iterate_categories(self.top_categories, level=1)
+
 
     def clean_name(self, name_text):
         match = re.match("(\d*): (\w*\.\d*\.\w*\.) (.*), Total", name_text)
@@ -36,9 +42,11 @@ class CodesGetter():
         match = re.match("(\d*): (.*), Total", name_text)
         return match.groups()[1]
 
+
     def clean_sector_name(self, name_text):
         match = re.match("(\d*): (.*)", name_text)
         return match.groups()[1]
+
 
     def get_relevant_parent(self, level, parent2_code, parent3_code, highest=True):
         if level == 5:
@@ -47,6 +55,7 @@ class CodesGetter():
             return parent2_code
         else:
             return parent2_code
+
 
     def iterate_categories(self, categories, level, parent_code={}, parent2_code={}, parent3_code={}):
         if parent2_code != {}:
@@ -70,23 +79,47 @@ class CodesGetter():
                     'group_name': self.clean_name(relevant_parent.get('name')),
                     'category_code': category.get('code'),
                     'category_name': self.clean_name(category.get('name')),
-                    'code': igtag,
-                    'name': self.clean_sector_name(title),
+                    'sector_code': igtag,
+                    'sector_name': self.clean_sector_name(title),
                 })
 
+
 def run():
-    codes_groups = CodesGetter(doc_crs_codes).codes
+    print("Running scraper...")
+    print("Retrieving FR codes...")
+    codes_groups_fr = CodesGetter(lang='fr').codes
+    dict_groups_fr = dict(map(lambda code: (code['sector_code'], code), codes_groups_fr))
+    print("Retrieved FR codes.")
+    print("Retrieving EN codes...")
+    codes_groups = CodesGetter().codes
+    print("Retrieved EN codes.")
+    print("Writing output...")
     os.makedirs("output", exist_ok=True)
     with open(os.path.join("output", "sectors_groups.csv"), 'w') as csv_f:
         csvwriter = csv.DictWriter(csv_f, fieldnames=[
-            'group_code', 'group_name',
-            'category_code', 'category_name',
-            'code', 'name'
+            'code', 'name_en', 'name_fr',
+            'category_code', 'category_name_en',
+            'category_name_fr',
+            'sector_code', 'sector_name_en',
+            'sector_name_fr'
         ])
         csvwriter.writeheader()
         for code in codes_groups:
-            csvwriter.writerow(code)
+            code_fr = dict_groups_fr[code['sector_code']]
+            row = {
+                'code': code['group_code'],
+                'name_en': code['group_name'],
+                'name_fr': code_fr['group_name'],
+                'category_code': code['category_code'],
+                'category_name_en': code['category_name'],
+                'category_name_fr': code_fr['category_name'],
+                'sector_code': code['sector_code'],
+                'sector_name_en': code['sector_name'],
+                'sector_name_fr': code_fr['sector_name']
+            }
+            csvwriter.writerow(row)
         if os.environ.get("GITHUB_PAGES", False) is False:
             scraperwiki.sqlite.save(unique_keys=['code'], data=codes_groups)
+    print("Done")
 
 run()
